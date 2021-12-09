fd7a56bf8408a0306643861fd9b1678ecf36c2db
LiamB017 <liambedford17@gmail.com>
2021-12-09 15:06:37 +0000

    Role Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,11 @@ class User extends Authenticatable
       return $this->belongsToMany('App\Models\Role', 'user_role');
   }
 
+  public function hasRole($role)
+  {
+    return null !== $this->roles()->where('name', $role)->first();
+ }
+
+  
 }
+
